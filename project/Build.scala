fa7b3f4bf60475151c0bab47adfea3425217e6e6
Taro L. Saito <leo@xerial.org>
2012-06-27 14:30:34 +0900

    Fix launch script

--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,4 @@
-package utgenome.sample
+package utgenome
 
 import sbt._
 import Keys._
@@ -8,14 +8,20 @@ object ProjectBuild extends Build {
    lazy val root = Project(
      id ="sample-project",
      base = file("."),
-     settings = Defaults.defaultSettings ++ Seq(
-       scalaVersion := "2.9.2",
-       organization := "org.utgenome.sample",
-       version := "1.0-SNAPSHOT",
-       libraryDependencies ++= Seq(
-          "org.scalatest" %% "scalatest" % "2.0.M1" % "test"
-       )
+     settings = 
+       Defaults.defaultSettings 
+       ++ Seq(PackageTask.packageDistTask) 
+       ++ PackageTask.distSettings 
+       ++ Seq(
+       	  scalaVersion := "2.9.2",
+	  organization := "org.utgenome.sample",
+       	  version := "1.0-SNAPSHOT",
+       	  scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
+    	  parallelExecution := true,
+    	  crossPaths := false,
+       	  libraryDependencies ++= Seq(
+             "org.scalatest" %% "scalatest" % "2.0.M1" % "test"
+	  )
      )
    )
-
 }
